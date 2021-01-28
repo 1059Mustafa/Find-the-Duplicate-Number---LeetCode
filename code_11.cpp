bbf7737a99e3a5e24da89b95ba7332047a0c4d3d
1059Mustafa <72117500+1059Mustafa@users.noreply.github.com>
2021-01-29 01:00:19 +0530

    Add files via upload

---
+++ code_11.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& arr) 
+    {
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[abs(arr[i])]>0)
+            {
+                arr[abs(arr[i])]=(-arr[abs(arr[i])]);
+            }
+            else
+            {
+                return abs(arr[i]);
+            }
+        }
+        return -1;
+        
+    }
+};
